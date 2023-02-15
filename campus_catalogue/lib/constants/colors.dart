2461bb7fe8edc54e26dd11721f260876a5283641
TESLA2402 <lakshay6447@gmail.com>
2023-02-15 22:11:00 +0530

    Added SignIn and Verification OTP screen

--- campus_catalogue/lib/constants/colors.dart
+++ campus_catalogue/lib/constants/colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color backgroundOrange = Color(0xFFFCBC58);
+  static const Color backgroundYellow = Color(0xFFFFFEF6);
+  static const Color signIn = Color(0xFFFFF2E0);
 }
